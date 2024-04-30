22ba0e9fbc2c05b4324f4d02298f305ea1f7f1d0
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-04-30 14:09:48 -0500

    Prep 5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program!");
+        
+        Console.Write("Please enter your name.");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        int favNum = int.Parse(Console.ReadLine());
+        double square = Math.Pow(favNum, 2);
+
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
