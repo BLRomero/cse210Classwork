c375ce61fcd0a7f1b53f74aace8be40e5de9d87c
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-05-16 10:29:17 -0500

    Quitting the program works.

--- prove/Develop02/Quit.cs
+++ prove/Develop02/Quit.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 
 public class Quit
 {
-    public static void quit()
+    public static void _quit()
     {
-        Console.WriteLine("This is the Quit function");
+        Console.WriteLine("Are you sure you want to exit the application?");
+         Console.ReadLine();
+         Console.WriteLine("Press the Enter key to exit the program.");
          Console.ReadLine();
     }
 }
