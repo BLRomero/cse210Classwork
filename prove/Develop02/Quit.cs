2130d10fe841805d3cb8029e06f86144d242d94b
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-05-16 10:40:57 -0500

    I like this exit better  less code,  simpler

--- prove/Develop02/Quit.cs
+++ prove/Develop02/Quit.cs
@@ -5,9 +5,7 @@ public class Quit
 {
     public static void _quit()
     {
-        Console.WriteLine("Are you sure you want to exit the application?");
-         Console.ReadLine();
-         Console.WriteLine("Press the Enter key to exit the program.");
-         Console.ReadLine();
+        Environment.Exit(0);
+        
     }
 }
