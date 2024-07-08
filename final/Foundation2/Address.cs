c0aa29d2df9e60978632f0e67c717e22a695f113
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-08 14:29:29 -0500

    address.cs updated

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -2,28 +2,26 @@ using System;
 
 class Address
 {
-    protected string _streetAddress;
-    protected string _city;
-    protected string _state;
-    protected string _country;
+    private string _streetAddress;
+    private string _city;
+    private string _state;
+    private string _country;
 
-
-    Address(string streetAddress, string city, string state, string country)
+    public Address(string streetAddress, string city, string state, string country)
     {
-
-        streetAddress = _streetAddress;
-        city = _city;
-        state = _state;
-        country = _country;
-
+        _streetAddress = streetAddress;
+        _city = city;
+        _state = state;
+        _country = country;
     }
 
-   public bool IsUSA(){
+    public bool isUSA()
+    {
         return _country.Equals("USA", StringComparison.OrdinalIgnoreCase);
     }
+
     public string GetFullAddress()
     {
-        return ($"{_streetAddress} {_city}, {_state}, {_country}");
-
+        return $"{_streetAddress}, {_city}, {_state}, {_country}";
     }
 }
