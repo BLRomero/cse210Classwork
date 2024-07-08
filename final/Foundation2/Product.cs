3426b07838a8ed173932ffd145b2ee77b04f9ffb
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-08 14:29:53 -0500

    product.cs updated

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -1,11 +1,38 @@
 using System;
+using System.Security.Cryptography;
 
-public class Product{
+public class Product
+{
 
-    protected string _name;
-    protected string _productID;
-    protected string _price;
-    protected string _quantity;
+    protected string _productName;
+    protected int _productID;
+    protected float _pricePerUnit;
+    protected int _quantity;
 
+    public Product(string productName, int productID, float pricePerUnit, int quantity)
+    {
+        _productName = productName;
+        _productID = productID;
+        _pricePerUnit = pricePerUnit;
+        _quantity = quantity;
+    } 
+    public string GetProductName(){
+        return _productName
+    }
+
+ public int GetProductID(){
+        return _productID;
+    }
+
+ public float GetPricePerUnit(){
+        return _pricePerUnit;
+    }
+     public int GetQuanity(){
+        return _quantity;
+    }
+public float GetTotalCost()
+    {
+        return _pricePerUnit * _quantity;
+}
 
 }
