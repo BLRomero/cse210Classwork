108130cda3d491b2507f1fc338bf8f595e2a7076
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-08 14:29:41 -0500

    customer.cs updated

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -1,11 +1,38 @@
 using System;
 
-class Customer{
-    protected string _name;
+class Customer
+{
+    private string _customerName;
+    private Address _address;
 
-       public string GetFullAddress()
+    public Customer(string name, Address address)
     {
-        return ($"{_streetAddress} {_city}, {_state}, {_country}");
+        _customerName = name;
+        _address = address;
+    }
 
+    public void AddCustomerName(string name)
+    {
+        _customerName = name;
+    }
+
+    public void SetAddress(Address address)
+    {
+        _address = address;
+    }
+
+    public Address GetAddress()
+    {
+        return _address;
+    }
+
+    public string GetCustomerName()
+    {
+        return _customerName;
+    }
+
+    public bool isInUSA()
+    {
+        return _address.isUSA();
     }
 }
