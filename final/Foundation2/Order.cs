64e298e1954e2a47ba26795e795bebf26d01efb4
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-08 14:41:07 -0500

    working on Order.cs

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,9 +1,30 @@
 using System;
-
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
 class Order
 {
-    
 
-public List<Product> products = new List<Product>();
+    private List<Product> _products;
+    private Customer _customer;
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+    public void addProduct(Product product)
+    {
+        _products.Add(product);
+    }
+    public float calculateTotalCost()
+    {
+        float _totalCost = 0;
+        foreach(var product in _products)
+        {
+            _totalCost += product.GetTotalCost();
+
+        }
+        return _totalCost;
+    }
+
 
 }
