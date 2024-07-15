533e50ce58adb51e13df193834388be92cdb60e9
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-15 15:41:03 -0500

    Swimming is working

--- final/Foundation4/Swimming.cs
+++ final/Foundation4/Swimming.cs
@@ -1,10 +1,31 @@
 using System;
 
-class Swimming{
-    protected int _laps;
+class Swimming : Activity
+{
+    private int _laps; 
 
-    public Swimming(){
+    public Swimming(DateTime date, int duration, int laps) : base(date, duration)
+    {
+        _laps = laps;
+    }
 
-        
+    public override float GetDistance()
+    {
+        return _laps * 50f / 1609.34f; 
+    }
+
+    public override float GetSpeed()
+    {
+        return GetDistance() / (Duration / 60f);
+    }
+
+    public override float GetPace()
+    {
+        return Duration / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} Swimming - Distance: {GetDistance():F1} miles, Speed: {GetSpeed():F1} mph, Pace: {GetPace():F2} min/mile";
     }
 }
