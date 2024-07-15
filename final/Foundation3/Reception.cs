93692275aaa2b62e96573d39ca54cf170c259941
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-15 14:28:05 -0500

    reception event is working

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -5,7 +5,13 @@ class Reception : Event
 {
 
     protected string _emailRSVP;
-    public Reception(string eventTitle, string description, DateTime date, DateTime time, string address):base (eventTitle, description,  date,  time, address)
+    public Reception(string eventTitle, string description, DateTime date, DateTime time, string address, string emailRSVP):base (eventTitle, description,  date,  time, address)
     {
+        _emailRSVP = emailRSVP;
+     }
+
+     public new void PrintFullDetails(){
+        base.PrintFullDetails();
+        Console.WriteLine($"RSVP to: {_emailRSVP}");
      }
 }
