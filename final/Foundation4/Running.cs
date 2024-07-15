d1d7571bc2299bfaa296a4155ac10d862171f1e6
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-15 15:40:21 -0500

    Running is working

--- final/Foundation4/Running.cs
+++ final/Foundation4/Running.cs
@@ -1,10 +1,31 @@
 using System;
 
-class Running{
+class Running : Activity
+{
+    private float _distance; 
 
-    protected float _distance;
+    public Running(DateTime date, int duration, float distance) : base(date, duration)
+    {
+        _distance = distance;
+    }
 
-    public Running(){
-        
+    public override float GetDistance()
+    {
+        return _distance;
+    }
+
+    public override float GetSpeed()
+    {
+        return GetDistance() / (Duration / 60.0f);
+    }
+
+    public override float GetPace()
+    {
+        return Duration / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} Running - Distance: {GetDistance():F1} miles, Speed: {GetSpeed():F1} mph, Pace: {GetPace():F2} min/mile";
     }
 }
