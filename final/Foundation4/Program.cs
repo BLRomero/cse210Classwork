26b50653ad5bf450948c51a5fda93714c5d9f515
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-15 15:46:46 -0500

    Code is owrking

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+       
+        Running runningActivity = new Running(new DateTime(2024, 11, 3), 30, 3);
+        Cycling cyclingActivity = new Cycling(new DateTime(2024, 11, 3), 45, 15);
+        Swimming swimmingActivity = new Swimming(new DateTime(2024, 11, 3), 60, 20);
+
+        Console.WriteLine(runningActivity.GetSummary());
+        Console.WriteLine(cyclingActivity.GetSummary());
+        Console.WriteLine(swimmingActivity.GetSummary());
     }
 }
