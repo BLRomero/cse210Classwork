bc95794be4a71e2789299c0063fb61521a39cd78
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-15 15:41:25 -0500

    activity class is working

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,10 +1,32 @@
 using System;
 
-class Activity{
-    protected DateTime _date;
-    protected int _duration;
+abstract class Activity
+{
+    private DateTime _date;
+    private int _duration; 
 
-    public Activity(){
+    public Activity(DateTime date, int duration)
+    {
+        _date = date;
+        _duration = duration;
+    }
 
+    public DateTime Date
+    {
+        get { return _date; }
+    }
+
+    public int Duration
+    {
+        get { return _duration; }
+    }
+
+    public abstract float GetDistance(); 
+    public abstract float GetSpeed();    
+    public abstract float GetPace();    
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} ({_duration} min)";
     }
 }
