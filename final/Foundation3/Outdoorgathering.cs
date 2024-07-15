5d4084a7493da9e4ca5a9a543746166978ba5c83
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-15 13:29:43 -0500

    fixing code

--- final/Foundation3/Outdoorgathering.cs
+++ final/Foundation3/Outdoorgathering.cs
@@ -3,12 +3,15 @@ using System.Threading;
 
 class OutdoorGathering : Event
 {
-
-    protected string _emailRSVP;
+    protected string _weather;
 
     public OutdoorGathering(string eventTitle, string description, DateTime date, DateTime time, string address) : base(eventTitle, description, date, time, address)
     {
 
+    }
+    public void FullDetails()
+    {
+        Console.WriteLine($"Event: {_eventTitle}\n{_description}\nDate & Time: {_date}, {_time}\n{_address}\n{_weather}");
     }
 
 
--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -3,6 +3,8 @@ using System.Threading;
 
 class Reception : Event
 {
+
+    protected string _emailRSVP;
     public Reception(string eventTitle, string description, DateTime date, DateTime time, string address):base (eventTitle, description,  date,  time, address)
     {
      }
