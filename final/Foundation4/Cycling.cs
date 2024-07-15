88aa5ac5246a7d27192a0f26607fb184953ea750
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-15 15:46:35 -0500

    Cycling is working

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -1,10 +1,30 @@
 using System;
 
-class Cycling{
+class Cycling : Activity
+{
+    private int _speed;
 
-    protected float _speed;
 
-    public Cycling(){
-        
+
+    public Cycling(DateTime date, int duration, int speed) : base(date, duration)
+    {
+        _speed = speed;
+    }
+    public override float GetDistance()
+    {
+        return _speed * (Duration / 60f);
+    }
+
+    public override float GetSpeed()
+    {
+       return GetDistance() / (Duration / 60f);
+    }
+    public override float GetPace()
+    {
+        return Duration / GetDistance();
+    }
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} Cycling - Distance: {GetDistance():F1} miles, Speed: {GetSpeed():F1} mph, Pace: {GetPace():F2} min/mile";
     }
 }
