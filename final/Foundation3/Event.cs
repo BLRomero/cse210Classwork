45d309fa202c7c4b6461baf98bd6fc1b06ce69fd
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-07-15 13:04:04 -0500

    setting up attributes

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Event
+{
+    protected string _eventTitle;
+    protected string _description;
+    protected DateTime _date;
+    protected DateTime _time;
+    protected string _address;
+    protected string _standardDetails;
+    protected string _FullDetails;
+
+    public Event(string Event, string descritption, DateTime date, DateTime time, string address)
+    {
+
+    }
+
+
+
+
+}
